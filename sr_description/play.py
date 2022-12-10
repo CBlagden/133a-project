be088c81aa20db528f86a0cb4e41c014fcf07044
Chase Blagden <chase.blagden@gmail.com>
2022-12-09 19:16:43 -0800

    Added first spline segment

--- sr_description/play.py
+++ sr_description/play.py
@@ -12,10 +12,26 @@ from GeneratorNode     import GeneratorNode
 from KinematicChain    import KinematicChain
 from TransformHelpers  import *
 from music import l_theme as NOTES
+from keys import KEYS as KEYS_MAPPING
+
+LEFT_NOTES = []
+RIGHT_NOTES = []
+for note in NOTES:
+    if note.finger == 'L':
+        LEFT_NOTES.append(note)
+    else:
+        RIGHT_NOTES.append(note)
+
 
 N_SHARED_JOINTS = 8
 N_FINGER_JOINTS = 4
 
+
+
+def spline(t, T, p0, pf):
+    p = p0 + (pf-p0) * (3*t**2/T**2 - 2*t**3/T**3)
+    v =      (pf-p0) * (6*t   /T**2 - 6*t**2/T**3)
+    return (p, v)
 
 def dedup(lst):
     lookup = set()
@@ -39,6 +55,12 @@ class Trajectory():
 
         self.q = np.zeros((len(self.joints), 1)).reshape((-1,1))
         self.err = np.zeros((10, 1))
+
+        self.th_p0 = self.thchain.fkin(np.zeros((self.thchain.dofs,1)))[0]
+        self.mf_p0 = self.mfchain.fkin(np.zeros((self.mfchain.dofs,1)))[0]
+
+        self.th_x_desired_prev = self.th_p0
+        self.mf_x_desired_prev = self.mf_p0
 
         self.q_centers = np.vstack([np.zeros((N_SHARED_JOINTS, 1)), FINGER_CENTERS, FINGER_CENTERS])
         self.lams = np.vstack([np.zeros((N_SHARED_JOINTS, 1)), 0 * np.ones((N_FINGER_JOINTS * 2, 1))])
@@ -82,22 +104,70 @@ class Trajectory():
     def jointnames(self):
         return self.joints
 
+    def get_closest_next_note_pos(self, t, finger: str) -> np.ndarray:
+        min_dist = 100
+        closest_note_idx = None
+        for i, note in enumerate(NOTES):
+            if note.start < t < note.start + note.duration:
+               closest_note_idx = i
+               break
+            elif t < note.start:
+                dist = abs(t - note.start)
+                if dist < min_dist:
+                    min_dist = dist
+                    closest_note_idx = i
+
+        cur_note = NOTES[closest_note_idx]
+        key_pos = KEYS_MAPPING[cur_note.note]
+        return np.array([key_pos.x, key_pos.y, key_pos.z]).reshape((3, 1))
+
     def evaluate(self, t, dt):
         
         # A basic trajectory: try to spin the fingertips in circles
 
         # Palm face down
-        pa_rd =   Rotz(np.pi) @ Rotx(np.pi/2)
+        pa_rd = Rotz(np.pi) @ Rotx(np.pi/2)
         pa_wd = exyz(1, 0, 1)
 
+        th_x_desired_nxt = self.get_closest_next_note_pos(t, 'L')
+        mf_x_desired_nxt = self.get_closest_next_note_pos(t, 'R')
 
-        
+        t = t - 3
+        # if t < 0:
+        #     (s0, s0_dot) = spline(t + 3, 3, 0, 1)
+        #     th_x_desired = (1 - s0) * self.th_x_desired_prev + s0 * th_x_desired_nxt
+        #     mf_x_desired = (1 - s0) * self.mf_x_desired_prev + s0 * mf_x_desired_nxt
+
+        #     th_v_desired = s0 * np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+        #     mf_v_desired = s0 * np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+        # else:
+        #     th_x_desired = np.array([100, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+        #     mf_x_desired = np.array([105, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+
+        #     th_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+        #     mf_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+
+        if t < 0:
+            # Construct the spline to the first note position
+            (s0, s0_dot) = spline(t + 3, 3, 0, 1)
+            th_x_desired = (1 - s0) * self.th_p0 + s0 * np.array([100, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+            mf_x_desired = (1 - s0) * self.mf_p0 + s0 * np.array([105, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+
+            th_v_desired = s0 * np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+            mf_v_desired = s0 * np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+        else:
+            th_x_desired = np.array([100, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+            mf_x_desired = np.array([105, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+
+            th_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+            mf_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+
         pa_x_desired = np.array([1]).reshape((1,1)) * 0.01
-        th_x_desired = np.array([100, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
-        mf_x_desired = np.array([105, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+        # th_x_desired = np.array([100, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
+        # mf_x_desired = np.array([105, 100, 2*np.sin(15*t)]).reshape((3,1)) * 0.01
         pa_v_desired = np.array([0]).reshape((1,1)) * 0.01
-        th_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
-        mf_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+        # th_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
+        # mf_v_desired = np.array([0,   0,   np.cos(t)]).reshape((3,1)) * 0.01
 
 
         # Grab the last joint value and task error.
